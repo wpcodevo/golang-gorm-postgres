7057d067f16998f8921a205c4cafee5d00dc4d92
Edem <ziddah.edem@gmail.com>
2022-08-08 12:13:17 +0000

    updated

--- models/user.model.go
+++ models/user.model.go
@@ -1,15 +1,16 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 )
 
 type User struct {
-	ID    uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
-	Name  string    `gorm:"type:varchar(255)"`
-	Email string    `gorm:"uniqueIndex"`
-}
-
-func (User) UsersTable() string {
-	return "users"
+	ID uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	// ID     uint   `gorm:"primary_key"`
+	Name      string `gorm:"type:varchar(255);not null"`
+	Email     string `gorm:"uniqueIndex;not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
--- readMe.md
+++ readMe.md
@@ -1,1 +1,5 @@
 # Build Golang RESTful API with Gorm, Gin and Postgres
+
+### How to Setup Golang GORM RESTful API Project with Postgres
+
+[How to Setup Golang GORM RESTful API Project with Postgres](https://codevoweb.com/setup-golang-gorm-restful-api-project-with-postgres/)
