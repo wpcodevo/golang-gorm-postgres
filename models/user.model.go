4631b948d1025d15aaad559d300bb772d789e908
Edem Ziddah <contact@codevoweb.com>
2022-08-08 18:10:32 +0000

    updated

--- models/user.model.go
+++ models/user.model.go
@@ -8,15 +8,15 @@ import (
 
 type User struct {
 	ID        uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
-	Name      string    `gorm:"type:varchar(255)"`
-	Email     string    `gorm:"uniqueIndex"`
-	Password  string
-	Role      string `gorm:"type:varchar(255)"`
-	Provider  string
-	Photo     string
-	Verified  bool
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	Name      string    `gorm:"type:varchar(255);not null"`
+	Email     string    `gorm:"uniqueIndex;not null"`
+	Password  string    `gorm:"not null"`
+	Role      string    `gorm:"type:varchar(255);not null"`
+	Provider  string    `gorm:"not null"`
+	Photo     string    `gorm:"not null"`
+	Verified  bool      `gorm:"not null"`
+	CreatedAt time.Time `gorm:"not null"`
+	UpdatedAt time.Time `gorm:"not null"`
 }
 
 func (User) UsersTable() string {
